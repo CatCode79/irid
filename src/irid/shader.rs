d3880cc7a5b0e904c51eff9670fc523d9465e15e
Iolo1979 <andrea.postal@gmail.com>
2021-05-03 19:41:21 +0200

    SpirV pre-compilation

---
+++ src/irid/shader.rs
@@ -0,0 +1,23 @@
+
+
+//= SHADER'S COMPILING AT RUNTIME ==================================================================
+
+fn _create_shader_module() {
+    let _vs_src = include_str!("shaders/shader.vert");
+    let _fs_src = include_str!("shaders/shader.frag");
+    /*    let mut compiler = shaderc::Compiler::new().unwrap();
+        let vs_spirv = compiler.compile_into_spirv(vs_src, shaderc::ShaderKind::Vertex, "shader.vert", "main", None).unwrap();
+        let fs_spirv = compiler.compile_into_spirv(fs_src, shaderc::ShaderKind::Fragment, "shader.frag", "main", None).unwrap();
+        let vs_data = wgpu::util::make_spirv(vs_spirv.as_binary_u8());
+        let fs_data = wgpu::util::make_spirv(fs_spirv.as_binary_u8());
+        let vs_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+            label: Some("Vertex Shader"),
+            source: vs_data,
+            flags: wgpu::ShaderFlags::default(),
+        });
+        let fs_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+            label: Some("Fragment Shader"),
+            source: fs_data,
+            flags: wgpu::ShaderFlags::default(),
+        });*/
+}
--- src/state.rs
+++ src/state.rs
@@ -61,23 +61,9 @@ impl State {
 
 		let clear_color = wgpu::Color::BLACK;
 
-		let vs_src = include_str!("shader.vert");
-		let fs_src = include_str!("shader.frag");
-		let mut compiler = shaderc::Compiler::new().unwrap();
-		let vs_spirv = compiler.compile_into_spirv(vs_src, shaderc::ShaderKind::Vertex, "shader.vert", "main", None).unwrap();
-		let fs_spirv = compiler.compile_into_spirv(fs_src, shaderc::ShaderKind::Fragment, "shader.frag", "main", None).unwrap();
-		let vs_data = wgpu::util::make_spirv(vs_spirv.as_binary_u8());
-		let fs_data = wgpu::util::make_spirv(fs_spirv.as_binary_u8());
-		let vs_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-			label: Some("Vertex Shader"),
-			source: vs_data,
-			flags: wgpu::ShaderFlags::default(),
-		});
-		let fs_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-			label: Some("Fragment Shader"),
-			source: fs_data,
-			flags: wgpu::ShaderFlags::default(),
-		});
+		let vs_module = device.create_shader_module(&wgpu::include_spirv!("shader.vert.spv"));
+		let fs_module = device.create_shader_module(&wgpu::include_spirv!("shader.frag.spv"));
+
 		let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
 			label: Some("Render Pipeline Layout"),
 			bind_group_layouts: &[],
