6581d1471b8eb7cae3f3d9038eeb108377e921e7
Iolo1979 <andrea.postal@gmail.com>
2021-06-22 11:54:34 +0200

    also this file

---
+++ irid/src/window/window.rs
@@ -0,0 +1,6 @@
+
+//= STRUCT ALIASES =================================================================================
+
+/// Identifier of a window. Unique for each window.
+/// Type alias for [`winit::window::WindowId`](winit::window::WindowId)
+pub type WindowId = winit::window::WindowId;
